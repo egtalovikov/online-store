959bc167714f169b62bdfd031483fd68269cabc6
Egor Talovikov <egtalovikov@gmail.com>
2023-08-08 01:45:55 +0500

    feat: реализовал middleware для проверки токена

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -43,11 +43,7 @@ class UserController {
     }
 
     async check(req, res, next) {
-        const {id} = req.query
-        if (!id) {
-            return next(ApiError.badRequest('Не задан ID'))
-        }
-        res.json(id)
+        
     }
 }
 
