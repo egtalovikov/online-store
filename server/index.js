053c091b7c73c196b96ea0fafce3ed32eb543a9b
Egor Talovikov <egtalovikov@gmail.com>
2023-08-07 17:13:22 +0500

    feat: добавил .env в gitignore

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 
-const PORT = 5001
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, () => console.log(`Server started on port ${5001}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
