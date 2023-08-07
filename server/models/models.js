0b69bf95399d53ee842b8c45da513f5dbe6cbb42
Egor Talovikov <egtalovikov@gmail.com>
2023-08-07 17:59:31 +0500

    feat: создал модель для устройства в корзине

--- server/models/models.js
+++ server/models/models.js
@@ -11,3 +11,7 @@ const User = sequelize.define('user', {
 const Basket = sequelize.define('basket', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 })
+
+const BasketDevice = sequelize.define('basket_device', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
