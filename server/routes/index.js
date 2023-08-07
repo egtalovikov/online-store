b3aa6dc7b76d622ee98141f85f29e8833b0a1ede
Egor Talovikov <egtalovikov@gmail.com>
2023-08-07 18:28:19 +0500

    feat: создал роуты

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+const Router = require('express')
+const router = new Router()
+const deviceRouter = require('./deviceRouter')
+const userRouter = require('./userRouter')
+const brandRouter = require('./brandRouter')
+const typeRouter = require('./typeRouter')
+
+
+router.use('/user',)
+router.use('/type',)
+router.use('/brand',)
+router.use('/device',)
+
+module.exports = router
