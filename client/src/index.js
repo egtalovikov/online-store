cf35e1c0d0106e9bf383638577f302c201221f6e
Egor Talovikov <egtalovikov@gmail.com>
2023-08-08 19:11:18 +0500

    feat: сверстал авторизацию и регистрацию

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,17 @@
-import React, { createContext } from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {createContext} from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
-import UserStore from './store/UserStore';
-import DeviceStore from './store/DeviceStore';
+import UserStore from "./store/UserStore";
+import DeviceStore from "./store/DeviceStore";
 
 export const Context = createContext(null)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
     <Context.Provider value={{
-      user: new UserStore(),
-      device: new DeviceStore(),
+        user: new UserStore(),
+        device: new DeviceStore(),
     }}>
-      <App />
-    </Context.Provider>
-  </React.StrictMode>
+        <App />
+    </Context.Provider>,
+  document.getElementById('root')
 );
