0a3266aecf5caefdd11e3e0aef0918546ecb6f8a
Egor Talovikov <egtalovikov@gmail.com>
2023-08-08 20:14:41 +0500

    feat: оживил кнопки в навигации

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,26 +3,39 @@ import { Context } from '..';
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import { NavLink } from 'react-router-dom';
-import { SHOP_ROUTE } from '../utils/consts';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 import { Button } from "react-bootstrap";
 import { observer } from "mobx-react-lite";
 import Container from "react-bootstrap/Container"
-
+import { useHistory } from 'react-router-dom';
 const NavBar = observer(() => {
     const { user } = useContext(Context)
+    const history = useHistory()
+
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavLink style={{ color: 'white' }} to={SHOP_ROUTE}>КупиДевайс</NavLink>
                 {user.isAuth ?
                     <Nav className="ms-auto" style={{ color: 'white' }}>
-                        <Button variant='outline-light'>Админ панель</Button>
-                        <Button variant='outline-light' className="ms-2">Войти</Button>
+                        <Button
+                            variant='outline-light'
+                            onClick={() => history.push(ADMIN_ROUTE)}
+                        >
+                            Админ панель
+                        </Button>
+                        <Button
+                            variant='outline-light'
+                            onClick={() => history.push(LOGIN_ROUTE)}
+                            className="ms-2"
+                        >
+                            Выйти
+                        </Button>
                     </Nav>
                     :
                     <Nav className="ms-auto" style={{ color: 'white' }}>
                         <Button variant='outline-light' onClick={() => user.setIsAuth(true)}>Авторизация</Button>
-                    </Nav> 
+                    </Nav>
                 }
             </Container>
         </Navbar>
