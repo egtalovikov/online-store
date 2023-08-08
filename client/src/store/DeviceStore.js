9c83706ac90f88c438c9dc22b8935ec19df7b301
Egor Talovikov <egtalovikov@gmail.com>
2023-08-08 19:23:45 +0500

    feat: создал панельку с типами

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -4,7 +4,9 @@ export default class DeviceStore {
     constructor() {
         this._types = [
             {id: 1, name: 'Холодильники'},
-            {id: 2, name: 'Смартфоны'}
+            {id: 2, name: 'Смартфоны'},
+            {id: 3, name: 'Ноутбуки'},
+            {id: 4, name: 'Телевизоры'},
         ]
         this._brands = [
             {id: 1, name: "Samsung"},
@@ -13,6 +15,7 @@ export default class DeviceStore {
         this._devices = [
             {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: 'https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png'}
         ]
+        this._selectedType = {}
         makeAutoObservable(this)
     }
 
@@ -25,6 +28,9 @@ export default class DeviceStore {
     setDevices(devices) {
         this._devices = devices
     }
+    setSelectedType(type) {
+        this._selectedType = type
+    }
 
     get types() {
         return this._types
@@ -34,5 +40,8 @@ export default class DeviceStore {
     }
     get devices() {
         return this._devices
+    }
+    get selectedType() {
+        return this._selectedType
     }
 }
