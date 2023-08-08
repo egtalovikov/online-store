80deadf0305d87d0e38ab05ee2967cb0ef4794a2
Egor Talovikov <egtalovikov@gmail.com>
2023-08-08 20:11:04 +0500

    feat: сверстал описание товара

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -4,7 +4,13 @@ import bigStar from '../assets/bigStar.png'
 
 const DevicePage = () => {
     const device = { id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: 'https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png' };
-
+    const description = [
+        {id: 1, title: 'Оперативная память', description: '5 гб'},
+        {id: 2, title: 'Камера', description: '12 мп'},
+        {id: 3, title: 'Процессор', description: 'Пентиум 3'},
+        {id: 4, title: 'Кол-во ядер', description: '2'},
+        {id: 5, title: 'Аккумулятор', description: '4000'},
+    ]
     return (
         <Container className="mt-3">
             <Row>
@@ -31,6 +37,14 @@ const DevicePage = () => {
                         <Button variant={"outline-dark"}>Добавить в корзину</Button>
                     </Card>
                 </Col>
+            </Row>
+            <Row className="d-flex flex-column m-3">
+                <h1>Характеристики</h1>
+                {description.map((info, index) => 
+                <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                    {info.title}: {info.description}
+                </Row>
+                    )}
             </Row>
         </Container>
     );
