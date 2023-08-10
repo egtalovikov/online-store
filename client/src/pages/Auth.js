188f676ba0a1a105d238c0c786c1202639091603
Egor Talovikov <egtalovikov@gmail.com>
2023-08-10 22:09:34 +0500

    feat: сделал редирект в случае успешной авторизации

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -3,28 +3,34 @@ import { Container, Form } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
-import { NavLink, useLocation } from 'react-router-dom';
-import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
+import { NavLink, useLocation, useHistory } from 'react-router-dom';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from '../utils/consts';
 import { login, registration } from '../http/userAPI';
 import { observer } from 'mobx-react-lite';
 import { Context } from '..';
 
 const Auth = observer(() => {
-    const {user} = useContext(Context)
+    const { user } = useContext(Context)
     const location = useLocation()
+    const history = useHistory()
     const isLogin = location.pathname === LOGIN_ROUTE
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     const click = async () => {
-        let data;
-        if (isLogin) {
-            data = await login(email, password)
-        } else {
-            data = await registration(email, password)
+        try {
+            let data;
+            if (isLogin) {
+                data = await login(email, password)
+            } else {
+                data = await registration(email, password)
+            }
+            user.setUser(data)
+            user.setIsAuth(true)
+            history.push(SHOP_ROUTE)
+        } catch (e) {
+            alert(e.response.data.message)
         }
-        user.setUser(data)
-        user.setIsAuth(true)
     }
 
     return (
