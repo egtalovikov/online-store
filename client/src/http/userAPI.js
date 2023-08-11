c4de38c31f2045bedb8055d88f7fe8c1e116aae3
Egor Talovikov <egtalovikov@gmail.com>
2023-08-11 16:12:23 +0500

    feat: реализовал сохранение токена при авторизации

--- client/src/http/userAPI.js
+++ client/src/http/userAPI.js
@@ -3,15 +3,17 @@ import jwt_decode from "jwt-decode";
 
 export const registration = async (email, password) => {
     const {data} = await $host.post('api/user/registration', {email, password, role: 'ADMIN'})
+    localStorage.setItem('token', data.token)
     return jwt_decode(data.token)
 }
 
 export const login = async (email, password) => {
     const {data} = await $host.post('api/user/login', {email, password})
+    localStorage.setItem('token', data.token)
     return jwt_decode(data.token)
 }
 
 export const check = async () => {
-    const response = await $host.post('api/auth/registration',)
+    const response = await $authHost.get('api/auth/registration',)
     return response
 }
