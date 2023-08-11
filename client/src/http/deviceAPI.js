c534681731d1f7c8139333e5aefc3f214f6fddd7
Egor Talovikov <egtalovikov@gmail.com>
2023-08-11 20:17:50 +0500

    feat: доработал получение устройств по страницам

--- client/src/http/deviceAPI.js
+++ client/src/http/deviceAPI.js
@@ -16,10 +16,8 @@ export const createBrand = async (brand) => {
     return data
 }
 
-export const fetchBrands = async (typeId, brandId, page, limit = 5) => {
-    const {data} = await $host.get('api/brand', {params: {
-        typeId, brandId, page, limit
-    }})
+export const fetchBrands = async () => {
+    const {data} = await $host.get('api/brand', )
     return data
 }
 
@@ -28,8 +26,10 @@ export const createDevice = async (device) => {
     return data
 }
 
-export const fetchDevices = async () => {
-    const {data} = await $host.get('api/device')
+export const fetchDevices = async (typeId, brandId, page, limit = 5) => {
+    const {data} = await $host.get('api/device', {params: {
+        typeId, brandId, page, limit
+    }})
     return data
 }
 
