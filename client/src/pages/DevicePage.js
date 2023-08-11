c27eac6e3ca7a7a6c8371a51748d913b0e3386a8
Egor Talovikov <egtalovikov@gmail.com>
2023-08-11 17:13:43 +0500

    feat: реализовал получение устройства

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Card, Col, Container, Image, Row } from 'react-bootstrap';
 import bigStar from '../assets/bigStar.png'
+import {useParams} from 'react-router-dom';
+import { fetchOneDevice } from '../http/deviceAPI';
 
 const DevicePage = () => {
-    const device = { id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: 'https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png' };
-    const description = [
-        {id: 1, title: 'Оперативная память', description: '5 гб'},
-        {id: 2, title: 'Камера', description: '12 мп'},
-        {id: 3, title: 'Процессор', description: 'Пентиум 3'},
-        {id: 4, title: 'Кол-во ядер', description: '2'},
-        {id: 5, title: 'Аккумулятор', description: '4000'},
-    ]
+    const [device, setDevice] = useState({info: []})
+    const {id} = useParams()
+    useEffect(() => {
+        fetchOneDevice(id).then(data => setDevice(data))
+    }, [])
+
     return (
         <Container className="mt-3">
             <Row>
                 <Col md={4}>
-                    <Image width={300} height={300} src={device.img} />
+                    <Image width={300} height={300} src={process.env.REACT_APP_API_URL + device.img} />
                 </Col>
                 <Col md={4}>
                     <Row className="d-flex flex-column align-items-center">
@@ -40,7 +40,7 @@ const DevicePage = () => {
             </Row>
             <Row className="d-flex flex-column m-3">
                 <h1>Характеристики</h1>
-                {description.map((info, index) => 
+                {device.info.map((info, index) => 
                 <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
                     {info.title}: {info.description}
                 </Row>
