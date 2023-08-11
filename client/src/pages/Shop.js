8c8cb403796ae04a8e746f2ce64d2cd266e50337
Egor Talovikov <egtalovikov@gmail.com>
2023-08-11 16:33:48 +0500

    feat: реализовал логику получения типов устройствами, удалил моковые данные

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import TypeBar from '../components/TypeBar';
 import BrandBar from '../components/BrandBar';
 import DeviceList from '../components/DeviceList';
+import { observer } from 'mobx-react-lite';
+import { Context } from '../index';
+import { fetchTypes } from '../http/deviceAPI';
 
-const Shop = () => {
+const Shop = observer(() => {
+    const {device} = useContext(Context)
+
+    useEffect(() => {
+        fetchTypes().then(data => device.setTypes(data))
+    }, [])
+
     return (
         <Container>
             <Row className="mt-2">
@@ -20,6 +29,6 @@ const Shop = () => {
             </Row>
         </Container>
     );
-};
+});
 
 export default Shop;
